61525bc5199694c768fe2083fc04cd3eb754946a
Ariel Mashraki <ariel@mashraki.co.il>
2014-08-22 22:11:53 +0300

    feat(common): create common.js file

---
+++ src/common.js
@@ -0,0 +1,14 @@
+/*jshint globalstrict:true*/
+'use strict';
+
+var isDefined = angular.isDefined,
+  isUndefined = angular.isUndefined,
+  isFunction = angular.isFunction,
+  isString = angular.isString,
+  isNumber = angular.isNumber,
+  isObject = angular.isObject,
+  isArray = angular.isArray,
+  forEach = angular.forEach,
+  extend = angular.extend,
+  copy = angular.copy,
+  equals = angular.equals;
