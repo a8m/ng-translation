91c1680a2e695c6ce21344dc354a935c033ecbd5
Ariel Mashraki <ariel@mashraki.co.il>
2014-08-26 18:43:47 +0300

    fix(demo): improve example

--- demo/demo.js
+++ demo/demo.js
@@ -2,14 +2,21 @@ angular.module('app', ['ng.static'])
   .controller('MainController', function($scope) {
 
     $scope.foo = 'bar';
+    $scope.role = 'manager';
+
+    $scope.changeRole = function(role) {
+      return (role === 'manager') ?
+        $scope.role = 'employee' :
+        $scope.role = 'manager';
+    };
 
   })
   .config(['ngStaticProvider', function(ngStaticProvider) {
     ngStaticProvider
-      .setBaseUrl('/ng-static/demo/static')
+      .setDirectory('/ng-static/demo/static')
       .staticFiles({
-        demo: '/demo.json',
-        demo1: '/demo.json',
-        demo2: '/demo.json'
+        demo:  'demo.json',
+        demo1: 'demo.json',
+        demo2: 'demo.json'
       })
   }]);
