7cc88045f61067268604745743425205b1d421cf
Ariel Mashraki <ariel@mashraki.co.il>
2014-08-26 18:04:34 +0300

    feat(common): strings helpers

--- src/common.js
+++ src/common.js
@@ -12,3 +12,25 @@ var isDefined = angular.isDefined,
   extend = angular.extend,
   copy = angular.copy,
   equals = angular.equals;
+
+/**
+ * @description
+ * returns if a string ends with a particular suffix
+ * @param str
+ * @param suffix
+ * @returns {boolean}
+ */
+function endsWith(str, suffix) {
+  return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
+/**
+ * @description
+ * returns if a string starts with a particular prefix
+ * @param str
+ * @param prefix
+ * @returns {boolean}
+ */
+function startWith(str, prefix) {
+  return !str.indexOf(prefix);
+}
