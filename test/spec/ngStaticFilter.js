c7e379502bfac60973bab232f5f313ff0a673e3e
Ariel Mashraki <ariel@mashraki.co.il>
2014-08-26 17:37:55 +0300

    test(spec): default test files

---
+++ test/spec/ngStaticFilter.js
@@ -0,0 +1,5 @@
+'use strict';
+
+describe('ngStaticFilter', function() {
+
+});
---
+++ test/spec/staticFilesLoader.js
@@ -0,0 +1,8 @@
+'use strict';
+
+describe('staticFileLoader', function() {
+
+
+  beforeEach(module('ng.static.files-loader'));
+
+});
