7cdb4602075b046c18975ebcd65370b1438512c9
Ariel Mashraki <ariel@mashraki.co.il>
2014-08-30 07:47:40 +0300

    test(ngStaticProvider): start unit test

--- test/spec/ngStaticProvider.js
+++ test/spec/ngStaticProvider.js
@@ -4,6 +4,9 @@ describe('ngStaticProvider', function() {
 
   beforeEach(module('ng.static.provider', function ($provide) {
     //mock dependency
+    $provide.value('staticFileLoader', {
+      get: function() {}
+    });
   }));
 
 });
