de0cd24ed8b06a94f2cc6de66584b9e640760e3b
Ariel Mashraki <ariel@mashraki.co.il>
2014-08-30 14:41:20 +0300

    test(ngStaticProvider): setters

--- test/spec/ngStaticProvider.js
+++ test/spec/ngStaticProvider.js
@@ -2,6 +2,33 @@
 
 describe('ngStaticProvider', function() {
 
+  //providers
+  function staticFiles(files) {
+    return function(ngStaticProvider) {
+      ngStaticProvider.staticFiles(files);
+    }
+  }
+
+  function addStaticFile(file) {
+    return function(ngStaticProvider) {
+      ngStaticProvider.addStaticFile(file);
+    }
+  }
+
+  function setFilesSuffix(sfx) {
+    return function(ngStaticProvider) {
+      ngStaticProvider.setFilesSuffix(sfx)
+    }
+  }
+
+  function setBaseUrl(utl) {
+    return function(ngStaticProvider) {
+      ngStaticProvider.setBaseUrl(url);
+    }
+  }
+
+  //mocks
+
   beforeEach(module('ng.static.provider', function ($provide) {
     //mock dependency
     $provide.value('staticFileLoader', {
