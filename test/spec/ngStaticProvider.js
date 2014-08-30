a0eba93cc2b25f3373b01fff68a49fb81fea3933
Ariel Mashraki <ariel@mashraki.co.il>
2014-08-30 19:59:08 +0300

    test(ngStaticProvider): test setters

--- test/spec/ngStaticProvider.js
+++ test/spec/ngStaticProvider.js
@@ -2,7 +2,9 @@
 
 describe('ngStaticProvider', function() {
 
-  //providers
+  var httpResult;
+
+  //Provider
   function staticFiles(files) {
     return function(ngStaticProvider) {
       ngStaticProvider.staticFiles(files);
@@ -21,19 +23,80 @@ describe('ngStaticProvider', function() {
     }
   }
 
-  function setBaseUrl(utl) {
+  function setBaseUrl(url) {
     return function(ngStaticProvider) {
       ngStaticProvider.setBaseUrl(url);
     }
   }
 
-  //mocks
+  //Mocks
+  function staticFilesLoaderMock($timeout, $q) {
+    //mock the http request
+    return {
+      get: function(options) {
+        var deferred = $q.defer();
+
+        $timeout(function() {
+          var object = {};
+          object[options.key] = httpResult[options.value];
+
+          deferred.resolve(object);
+        });
+        return deferred.promise;
+      }
+    };
+  }
+
+  //Actions
+  function setHttpResult(result) {
+    return httpResult = result;
+  }
 
   beforeEach(module('ng.static.provider', function ($provide) {
-    //mock dependency
-    $provide.value('staticFileLoader', {
-      get: function() {}
-    });
+    //Dependency as a mock
+    $provide.factory('staticFilesLoader', staticFilesLoaderMock);
   }));
+
+  describe('setters test', function() {
+
+    //staticFiles
+    it('should be able to set staticFiles', function() {
+      var files = { login: '/login_page',  logout: '/logout_page' };
+      module(staticFiles(files));
+      inject(function(ngStatic) {
+        expect(ngStatic.configuration.staticFiles).toEqual(files);
+      });
+    });
+
+    //addStaticFile
+    it('should able to add static file', function() {
+      var file = { foo: '/foo' };
+      module(addStaticFile(file));
+      inject(function(ngStatic) {
+        expect(ngStatic.configuration.staticFiles).toEqual(file);
+      });
+    });
+
+    //setBaseUrl
+    it('should add able to set base url', function() {
+      var url = 'app/static';
+      module(setBaseUrl(url));
+      inject(function(ngStatic) {
+        expect(ngStatic.configuration.baseUrl).toEqual(url);
+      });
+    });
+
+    //setDirectory
+    it('should add able to use setDirectory to set baseUrl', function() {
+      module(function(ngStaticProvider) {
+        ngStaticProvider
+          .setDirectory('/app/static');
+      });
+      inject(function(ngStatic) {
+        expect(ngStatic.configuration.baseUrl).toEqual('/app/static');
+      })
+    });
+
+  })
 
 });
