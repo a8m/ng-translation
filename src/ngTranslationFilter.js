06ff4c0e206424ce7567ccc0de7cd363d4aa9276
Ariel Mashraki <ariel@mashraki.co.il>
2014-10-20 00:11:10 +0300

    refac(ngTranslationFilter):

--- src/ngTranslationFilter.js
+++ src/ngTranslationFilter.js
@@ -11,12 +11,26 @@
  */
 
 angular.module('ng-translation.filter', [ 'ng-translation.provider' ])
-  .filter('translate', ['$parse', 'ngTranslation', function($parse, ngTranslation) {
+  .filter('translate', ['$parse', '$interpolate', 'ngTranslation', function($parse, $interpolate, ngTranslation) {
 
-    return function(string, value) {
+    return function(string) {
 
-      return $parse(string)(ngTranslation.getUsed()) || string;
+      var args = Array.prototype.slice.call(arguments, 1);
+      var funcName = isString(args[0]) ? 'get' : 'getUsed';
+      var res = $parse(string)(ngTranslation[funcName](args[0]));
 
+      //if there is no arguments
+      if(!args.length) {
+        return res || string;
+        //if the first argument is an object
+      } else if(isObject(args[0])) {
+        return $interpolate(res)(args[0])
+      }
+      //the first arguments is a string
+      //check if it should be interpolate
+      return isObject(args[1])
+        ? $interpolate(res)(args[1])
+        : res
     }
 
   }]);
--- test/spec/ngTranslationFilter.js
+++ test/spec/ngTranslationFilter.js
@@ -13,6 +13,9 @@ describe('ngTranslationFilter', function() {
     $provide.value('ngTranslation', {
       getUsed: function() {
         return mockFile;
+      },
+      get: function(lang) {
+        return mockFile[lang];
       }
     });
   }));
@@ -34,14 +37,47 @@ describe('ngTranslationFilter', function() {
       expect(translateFilter('employee')).toBe('hello employee');
 
       expect(translateFilter('msg.employee')).toBe('c ya employee');
-    }
-  ));
+  }));
 
   it('should return the given key if the value is undefined', inject(
     function(translateFilter) {
       expect(translateFilter('employer')).toBe('employer');
       expect(translateFilter('employer.btn')).toBe('employer.btn');
-    }
-  ));
+  }));
+
+  it('should get a file as an argument and fetch from this file', inject(
+    function(translateFilter) {
+      bindKeys({
+        de: { foo: 'barDe', baz: 'bugDe' },
+        en: { foo: 'barEn', baz: 'bugEn' }
+        });
+      expect(translateFilter('foo')).toBe('foo');
+      expect(translateFilter('foo', 'de')).toBe('barDe');
+      expect(translateFilter('foo', 'de')).toBe('barDe');
+      expect(translateFilter('foo', 'en')).toBe('barEn');
+      expect(translateFilter('foo', 'en')).toBe('barEn');
+  }));
+
+  it('should get an object as an argument, and interpolate', inject(
+    function($rootScope, translateFilter) {
+      $rootScope.name = 'Ariel';
+      $rootScope.age = 26;
+      var notScope = { name: 'Null', age: 'Infinity' };
+      bindKeys({ exp: 'Name: {{ name }}, Age: {{ age }}' });
+
+      expect(translateFilter('exp', $rootScope)).toBe('Name: Ariel, Age: 26');
+      expect(translateFilter('exp', notScope)).toBe('Name: Null, Age: Infinity');
+  }));
+
+  it('should get a file as the first arguments, and object for interpolation' +
+    'as second argument', inject(function(translateFilter) {
+    bindKeys({
+      de: { exp: 'De-Name: {{ name }}, De-Age: {{ age }}' },
+      en: { exp: 'En-Name: {{ name }}, En-Age: {{ age }}' }
+    });
+    var me = { name: 'Ariel', age: 26 };
+    expect(translateFilter('exp', 'de', me)).toBe('De-Name: Ariel, De-Age: 26');
+    expect(translateFilter('exp', 'en', me)).toBe('En-Name: Ariel, En-Age: 26');
+  }));
 
 });
