72320ef712514e80bf8ecaac6c197ef9b0c800b7
Ariel Mashraki <ariel@mashraki.co.il>
2014-10-22 09:23:34 +0300

    fix unused func

--- src/common.js
+++ src/common.js
@@ -1,17 +1,11 @@
 /*jshint globalstrict:true*/
 'use strict';
 
-var isDefined = angular.isDefined,
-  isUndefined = angular.isUndefined,
-  isFunction = angular.isFunction,
+var isUndefined = angular.isUndefined,
   isString = angular.isString,
-  isNumber = angular.isNumber,
   isObject = angular.isObject,
-  isArray = angular.isArray,
   forEach = angular.forEach,
-  extend = angular.extend,
-  copy = angular.copy,
-  equals = angular.equals;
+  extend = angular.extend;
 
 /**
  * @description
